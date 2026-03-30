93e25a1c933e9d12f5d11573f0332603d6f6fa7a
Cailu Caz <caiolucas.sn@gmail.com>
2026-03-30 16:48:39 -0300

    Update ExTuplas

--- ExTuplas.py
+++ ExTuplas.py
@@ -1,7 +1,7 @@
 # =========================================
 # EXERCÍCIOS - PARTE 1 (TUPLAS)
 # =========================================
-
+import os
 # -----------------------------------------
 # QUESTÃO 1 - Funcionário mais antigo
 # -----------------------------------------
@@ -98,48 +98,187 @@ def bowser(n_jogos,classificacao):
 # =========================================
 # EXERCÍCIOS - PARTE 2 (SISTEMA DE BOLÃO)
 # =========================================
-
+def bolao():
+    jogadores = [];
+    apostas = [];
+    print("Bem vindo ao Bolão!");
+    ordem = input(menu());
+    
+    while ordem != "0":
+        limparTela();
+        if ordem == "1":
+            cadastrarJogador(jogadores);
+        elif ordem == "2":
+            visualizarJogadores(jogadores);
+        elif ordem == "3":
+            cadastrarAposta(jogadores, apostas);
+        elif ordem == "4":
+            visualizarApostas(jogadores, apostas);
+        elif ordem == "5":
+            sorteio(jogadores, apostas);
+        else:
+            print("Opção inválida. Tente novamente.");
+        ordem = input(menu)
+    print("Bolão do Pésão desativado - Obrigado por Jogar!");
 # -----------------------------------------
 # QUESTÃO 1 - Menu principal
 # -----------------------------------------
+def menu():
+    print('''
+Bolão do Pésão - Escolha uma opção:
 
+1) Cadastrar usuário
+2) Visualizar usuários
+3) Cadastrar aposta
+4) Visualizar apostas
+5) Inserir sorteio e ver vencedores
+0) Sair 
+Digite o Número!
+''')
 # -----------------------------------------
 # FUNÇÃO AUXILIAR - Limpar tela
 # -----------------------------------------
-
+def limparTela():
+    if os.name == "nt":
+        os.system("cls");
+    else:
+        os.system("clear");
 # -----------------------------------------
 # QUESTÃO 2 - Cadastrar jogador
 # -----------------------------------------
-# Inserir (nome, CPF) se não existir
+# Inserir (nome, CPF) se não existir    
+def JaExiste(cpf, jogadores):
+    for (id,_) in jogadores:
+        if (id == cpf):
+            return True;
+    return False;
 
+def cadastrarJogador(jogadores):
+    nome = input("Digite o nome: ");
+    cpf = input("Digite o CPF: ");
+    
+    if(JaExiste(cpf,jogadores)):
+        print("Indivíduo já cadastrado! Cadastro não realizado.");
+    else:
+        jogadores.append(cpf , nome);
 # -----------------------------------------
 # QUESTÃO 3 - Visualizar jogadores
 # -----------------------------------------
-
+def visualizarJogadores(jogadores):
+    if (len(jogadores) == 0):
+        print("Nenhum jogador foi cadastrado.");
+    else:
+        i = 1;
+        print("#" + "--" + "Lista de Jogadores" + "--" +"#");
+        for (cpf, nome) in jogadores:
+            print(f'Jogador Nº{i}: {nome} - {cpf}  ');
+            i += 1;
 # -----------------------------------------
 # QUESTÃO 4 - Cadastrar aposta
 # -----------------------------------------
-
+def cadastrarAposta(jogadores, apostas):
+    apostadores = lerCPFs(jogadores);
+    numeroAposta = lerNumerosAposta();
+    apostas.append((apostadores , numeroAposta));
 # Sub-rotina: ler CPFs dos jogadores
-
+def lerCPFs(jogadores):
+    apostadores = [];
+    num = int(input("Quantos jogadores estão na Aposta?\n"));
+    while num < 1 or n > len(jogadores):
+        num = int(input("Número Inaceitável. Por favor tente novamente:\n"));
+    while len(apostadores) != num:
+        visualizarJogadores(jogadores);
+        cpf = input("Por Favor, Digite um CPF:\n");
+        if JaExiste(cpf,jogadores) and cpf not in apostadores:
+            apostadores.append(cpf);
+        else:
+            print("CPF Recusado, Tente novamente mais Tarde!");
+    return apostadores;
 # Sub-rotina: ler números da aposta
-
+def lerNumerosAposta():
+    qnt = int(input("Digite quantos números deseja colocar na Aposta:\n"));
+    seq = [];
+    while (qnt < 6 or qnt > 15):
+       qnt = int(input("Número Inaceitável. Por favor tente novamente:\n"));
+    while (len(seq) < qnt):
+        n = int(input("Digite um número para ser apostado neste cartão:\n"))
+        if (n >= 1 and n <= 60 and n not in seq):
+            seq.append(n);
+        else:
+            print("Número Recusado, Tente novamente mais Tarde!");
+            
+    return seq;
+    
 # -----------------------------------------
 # QUESTÃO 5 - Visualizar apostas
 # -----------------------------------------
-
+def visualizarApostas(jogadores, apostas):
+    if (len(apostas) == 0):
+        print("Nenhuma aposta foi cadastrada, Por Favor, Tente novamente mais tarde!");
+    else:
+        print("#" + "--" + "Lista de Apostas" + "--" +"#");
+        n = 1
+        for (id, num) in apostas:
+            print(f"Aposta Nº{i}\n");
+            print("Números da Aposta: ", end=" ");
+            for i in num:
+                print(f'[{num}]', end="-");
+            print();
+            print("Jogadores Participantes:");
+            for cpfs in id:
+                nome = identificarNome(cpfs,jogadores);
+                print(f"{nome} - (CPF: {cpfs})")
+            i += 1
+            line = "-"*8;
+            print(line);
+def identificarNome(cpf,jogadores):
+    for (id, num) in jogadores:
+        if id == cpf: 
+            return num;
+    return None;
+        
 # -----------------------------------------
 # QUESTÃO 6/7 - Resultado e vencedores
 # -----------------------------------------
-
-# Ler números sorteados
-
-# Verificar se lista está contida em outra
-
-# Contar apostas vencedoras
-
 # Listar vencedores e prêmios
-
+def sorteio(jogadores, apostas): 
+    n_sorteados = numerosSorteados();
+    valor_total_premio = float(input("Digite o valor total do prêmio do bolão:\n"));
+    vencedores = contWinners(apostas,n_sorteados);
+    premio_bilhete = valor_total_premio / len(vencedores);
+    n = 1;
+    print("#" + "--" + "Lista de Vencedores" + "--" +"#");
+    for aposta in vencedores:
+        premio_individual = premio_bilhete / len(aposta);
+        print(f'Aposta Nº{n}:\n');
+        for cpf in aposta:
+            nome = JaExiste(cpf, jogadores);
+            print(f"{nome} - (CPF: {cpf}) - Ganhou: R${premio_individual:.2f}")
+        n += 1;  
+# Ler números sorteados
+def numerosSorteados():
+    seq = [];
+    while (len(seq) < 6):
+        n = int(input("Digite o número sorteado:"));
+        if( n >= 1 and n <= 60 and n not in seq):
+            seq.append(n);
+        else:
+            print("Número Inaceitável, Tente Novamente.");
+    
+    return seq
+# Verificar se lista está contida em outra
+def verificationList(l1,l2):
+    for ele in l1:
+        if ele not in l2:
+            return False;
+    return True;
+# Contar apostas vencedoras
+def contWinners(apostas, n_sorteados):
+    vencedores = [];
+    for (ids,numeros) in apostas:
+        if (verificationList(n_sorteados, numeros)):
+            vencedores.append(ids);
+    return vencedores
 # -----------------------------------------
 # QUESTÃO 8 - Jogo Genius
 # -----------------------------------------
